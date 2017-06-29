2f6500287d52a1f9053084ea3c99ad305ff0f533
Tapio <tapix92@gmail.com>
2017-06-29 11:21:56 +0300

    MuuntamoWall: Added events for every minimum 30 minute slot that is not reserved and closed message for weekends.

--- muuntamoWall.php
+++ muuntamoWall.php
@@ -23,8 +23,8 @@ if(isset($_GET['nogui'])){
 }
 function retrieveWeek($start_date,$end_date){
 	//returns events from the time between $start_date and $end_date
-	$result="";
 	$thisWeek="";
+	$currentWeekDay = date('w', strtotime($start_date));
 	$start_date = explode(" ", $start_date);
 	$start_date = timeForDatabase($start_date[0],$start_date[1]);
 	$end_date = explode(" ", $end_date);
@@ -37,41 +37,102 @@ function retrieveWeek($start_date,$end_date){
 		//goes through all the events
 		$publictime="";
 		$reservationtime="";
-		//I had trouble with Date objects, so I ended up treating dates as strings...
+		//I had trouble with Date objects (Timezone conversions), so I ended up treating dates as strings...
 		//Time from database is in UTC
 		//Shown time should be in Europe/Helsinki
 		$start_date_temp = explode(" ", $row['start_date']);//Format:YYYY-MM-DD HH:SS
 		$end_date_temp = explode(" ", $row['end_date']);
 		$row['start_date'] = timeFromDatabase($start_date_temp[0],$start_date_temp[1]);
 		$row['end_date'] = timeFromDatabase($end_date_temp[0],$end_date_temp[1]);
-		$end_date_temp = explode(" ", $row['end_date']);
-		$end_date_temp = explode(":", $end_date_temp[1]);
-		$end_date_temp = $end_date_temp[0].":".$end_date_temp[1];
-		$start_date_temp = explode(" ", $row['start_date']);
-		$start_time_temp = explode(":", $start_date_temp[1]);
-		$start_time_temp = $start_time_temp[0].":".$start_time_temp[1];
-		$reservationtime=$start_time_temp." - ".$end_date_temp."<br/>";
-		$result=$result."<div class='eventBox'>\n";
+		$timeStartTemp = date("H:i",strtotime($row['start_date']));
+		$timeEndTemp = date("H:i",strtotime($row['end_date']));
+		$reservedTimes[] = $timeStartTemp."-".$timeEndTemp;
+		$end_date_temp = $timeEndTemp;
+		$start_time_temp = $timeStartTemp;
+		$reservationtime=$start_time_temp." - ".$timeEndTemp;
 		
+				//echo $reservationtime."<br>";
 		if(isset($row['PublicStartTime'])&&isset($row['PublicEndTime'])&&$row['PublicStartTime']!="00:00:00"){
 				$public_start_time_temp = explode(":", $row['PublicStartTime']);
 				$public_start_time_temp = $public_start_time_temp[0].":".$public_start_time_temp[1];
 				$public_end_time_temp = explode(":", $row['PublicEndTime']);
 				$public_end_time_temp = $public_end_time_temp[0].":".$public_end_time_temp[1];
-				$reservationtime="";
-				$publictime=$publictime.$public_start_time_temp." - ".$public_end_time_temp."<br/>\n";
+				//$reservationtime=$public_start_time_temp." - ".$public_end_time_temp;
+				$publictime=$public_start_time_temp." - ".$public_end_time_temp;
 		}else{
-			$publictime="Tapahtuma on yksityinen";
-			$row['description']="";
+			$publictime=FALSE;
+			$row['description']="Tapahtuma on yksityinen";
 		}
-		$result=$result."\t<h2 class='eventBox'>".$row['title']."</h2><p class='eventBox'>".$reservationtime.$publictime.$row['description']."</p>\n";
-	
-		$result=$result."\t<hr class='eventBox' align='left'/>\n</div>\n";
+		$result[] = eventGenerator($row,$reservationtime,$publictime);
 	}
-	$resultarray[0]=$result;
-	$resultarray[1]=$eventcount;
+	//Creates events for evey available time slot
+	$previouslystopped=TRUE;
+	if(isset($reservedTimes)){
+		for($i=date("H:i",strtotime("08:00"));$i<=date("H:i",strtotime("16:00"));$i=date("H:i", strtotime("+30 minutes",strtotime($i)))){
+			$stopped=FALSE;
+			foreach($reservedTimes as $temp){
+				$temp=explode("-",$temp);
+				if($i>=date("H:i",strtotime($temp[0]))&&$i<=date("H:i",strtotime($temp[1]))){
+					$stopped=TRUE;
+				}
+			}
+			if(!$stopped){
+				$endfreetime=$i;
+				if($previouslystopped){
+					$startfreetime=$i;
+					$previouslystopped=false;
+				}
+			}else{
+				if(isset($startfreetime)&&$startfreetime!=$endfreetime){
+					if(!isset($freetimes)||!in_array($startfreetime." - ".$endfreetime,$freetimes)){
+						$freetimes[]=$startfreetime." - ".$endfreetime;
+					}
+				}
+				$previouslystopped=true;
+			}
+			if($i==date("H:i",strtotime("16:00"))&&!$stopped&&isset($startfreetime)){
+					$freetimes[]=$startfreetime." - ".$endfreetime;
+			}
+		}
+	}else{
+		$freetimes[]="08:00 - 16:00";
+	}
+	if(isset($freetimes)&&$currentWeekDay!=6&&$currentWeekDay!=0){
+		foreach($freetimes as $temp){
+			$row['title']="Muuntamo vapaassa käytössä";
+			$row['description']="Käy rohkeasti sisään tai nauti ulkopeleistä!";
+			$result[] = eventGenerator($row,$temp,"");
+		}
+	}
+	if($currentWeekDay==6||$currentWeekDay==0){
+			$row['title']="Muuntamo kiinni";
+			$row['description']="Ulkotilat ovat vapaasti käytettävissäsi, nähdään taas arkena!";
+			$result[] = eventGenerator($row,"","");
+	}
+	
+	$resultarray[0]=$result;		//array containing all of the events
+	$resultarray[1]=$eventcount;	//How many events there are
+	
 	return $resultarray;
 }	
+
+//should do events with objects
+	function eventGenerator($row,$reservationtime,$publictime){
+		//generates html for event
+		if(isset($reservationtime)){
+			$event[0]=explode(" - ",$reservationtime);
+		}
+		if($publictime){
+			$reservationtime="";
+		}
+		$event[1]="
+		<div class='eventBox'>\n
+			\t<h2 class='eventBox'>".$row['title']."</h2>
+		<p class='eventBox'>".$reservationtime.$publictime."<br/>".$row['description']."</p>\n
+		\t<hr class='eventBox' align='left'/>\n
+		</div>\n";
+		return $event;
+	}
 //source for weekstart/weekend: https://stackoverflow.com/a/11905818/7785270
 
 	$week_start="2017-06-05 00:00:00";//incase nothing gets set
@@ -110,7 +171,18 @@ if($currentWeekDay==1){
 	$selectedDateString="";
 }
 $resultArray=retrieveWeek($week_start,$week_end);
-$thisWeek=$resultArray[0];
+$thisWeek="";
+
+function date_compare($a, $b)
+{
+    $t1 = strtotime($a[0][0]);
+    $t2 = strtotime($b[0][0]);
+    return $t1 - $t2;
+}    
+usort($resultArray[0], 'date_compare');
+foreach($resultArray[0] as $temp){
+	$thisWeek=$thisWeek.$temp[1];
+}
 $temp=explode(" ",$week_start);
 $dateBefore=date('Y-m-d', strtotime($temp[0]. ' - 1 days'));
 $dateAfter=date('Y-m-d', strtotime($temp[0]. ' + 1 days'));
